c9ec8f5c771b585553ec5e2e63c71eda36414dae
lavoor <stanislawsynall@gmail.com>
2025-01-10 16:24:46 +0100

    Update index.tsx

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <div className="w-full bg-blue flex flex-col justify-center items-center px-4 box-border mt-56 h-24">
-            <span className="text-white text-base sm:text-lg md:text-xl font-medium tracking-tight text-center">stanislawsynal.pl © All rights reserved 2024</span>
+            <span className="text-white text-base sm:text-lg md:text-xl font-medium tracking-tight text-center">stanislawsynal.pl © All rights reserved 2025</span>
         </div>
     )
 }
